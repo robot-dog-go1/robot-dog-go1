4423b6c27491566b046cc97b8389d015bf2edf09
Luciano Scarpaci <tauxsik@gmail.com>
2023-07-21 18:00:02 -0400

    Add Hopping Mode

--- index.js
+++ index.js
@@ -23,46 +23,52 @@ class myClient {
       }
     }
     
-    
     async onMessage() {
       //WALKING
       const SLOW_SPEED = 0.10000000000000000;
       const MED_SPEED = 0.50000000000000000;
       const FAST_SPEED = 0.90000000000000000;
       
+      // FORWARD
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.walk);
         await this.robotDog.goForward(SLOW_SPEED, 100);
       }
-      
+      // BACKWARD
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.walk);
         await this.robotDog.goBackward(SLOW_SPEED, 100);
       }
-      
+      // LEFT
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.walk);
         await this.robotDog.goLeft(SLOW_SPEED, 100);
       }
-
+      // RIGHT
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.walk);
         await this.robotDog.goRight(SLOW_SPEED, 100);
       }
-
+      // LEFT
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.walk);
         await this.robotDog.goLeft(SLOW_SPEED, 100);
       }
-      //TURNING
+      
+      //TURN LEFT RIGHT
+      
       await this.robotDog.wait(1000);
       await this.robotDog.turnLeft(FAST_SPEED, 1000);
       await this.robotDog.turnRight(MED_SPEED, 2000);
+      
+      // HOP HERE
+      
+     
       //PUSH UPS
       for (let i = 0; i < 4; i++) {
         this.robotDog.setMode(Go1Mode.standDown);
@@ -70,51 +76,78 @@ class myClient {
         this.robotDog.setMode(Go1Mode.standUp);
         await this.robotDog.wait(2000);
       }
+      // HOP HERE
+      await this.robotDog.wait(3000);
+      await this.robotDog.resetBody();
+      for (let i = 0; i < 2; i++) {
+        await this.robotDog.wait(1000);
+        this.robotDog.setMode(Go1Mode.walk);
+        await this.robotDog.goForward(MED_SPEED, 100);
+        await this.robotDog.resetBody();
+      }
       //WALKING IN ANOTHER WAY
-      
+      // LOOK UP
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.stand);
         await this.robotDog.lookUp(FAST_SPEED, 100);
       }
+      // LOOK DOWN
       await this.robotDog.wait(5000);
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.stand);
         await this.robotDog.lookDown(FAST_SPEED, 100);
       }
+      // LEAN LEFT
       await this.robotDog.wait(5000);
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.stand);
         await this.robotDog.leanLeft(FAST_SPEED, 100);
       }
+      // LEAN RIGHT
       await this.robotDog.wait(5000);
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.stand);
         await this.robotDog.leanRight(FAST_SPEED, 100);
       }
+      // TWIST LEFT
       await this.robotDog.wait(5000);
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.stand);
         await this.robotDog.twistLeft(MED_SPEED, 100);
       }
+      // TWIST RIGHT
       await this.robotDog.wait(5000);
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.stand);
         await this.robotDog.twistRight(MED_SPEED, 100);
       }
+      //HOP HERE DOESNT WORK
       //BEGGING MODE
-      await this.robotDog.wait(1000);
+      await this.robotDog.wait(3000);
       for (let i = 0; i < 2; i++) {
         await this.robotDog.wait(1000);
         this.robotDog.setMode(Go1Mode.straightHand1);
-
+        await this.robotDog.wait(3000);
       }
-
+      
+     //HOPPING
+      await this.robotDog.wait(3000);
+      await this.robotDog.resetBody();
+      for (let i = 0; i < 2; i++) {
+        await this.robotDog.wait(1000);
+        this.robotDog.setMode(Go1Mode.walk);
+        await this.robotDog.goForward(MED_SPEED, 100);
+        await this.robotDog.resetBody();
+      }
+      
+      
+    
     }
 }
 
